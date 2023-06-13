c04f70c71173eaeba8f364d704160adb9c7bf2ab
marta.verdugo <marta.verdugo@nanoporetech.com>
2023-06-13 11:50:36 +0100

    added postgres database

---
+++ app/app/calc.py
@@ -0,0 +1,12 @@
+"""
+Calculator functions
+"""
+
+
+def add(number_1, number_2):
+    """ Add number_1 and number2 and return result"""
+    return number_1 + number_2
+
+def subtract(number_1, number_2):
+    """Subtract number_1 and number2 and return result"""
+    return number_1 - number_2
