0806c565520c209f537253048a0a5129be468774
marta.verdugo <marta.verdugo@nanoporetech.com>
2023-09-19 12:46:03 +0100

    linting

--- app/app/calc.py
+++ app/app/calc.py
@@ -7,6 +7,7 @@ def add(number_1, number_2):
     """ Add number_1 and number2 and return result"""
     return number_1 + number_2
 
+
 def subtract(number_1, number_2):
     """Subtract number_1 and number2 and return result"""
     return number_1 - number_2
